1645087741a8786c9ccdc8f163e5dae663b1b684
Akshay <5638.akshay@gmail.com>
2024-05-18 18:12:42 +0530

    Initial package layout

---
+++ src/Ai/index.js
@@ -0,0 +1,5 @@
+export {default as ClAirbnb} from './AiAirbnb';
+export {default as AiZoomZoom} from './AiZoom';
+export {default as AiZerpply} from './AiZerpply';
+export {default as AiZendesk} from './AiZendesk';
+export {default as AiYoutube} from './AiYoutube';
